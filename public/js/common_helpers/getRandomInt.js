f18d852877a7b9f83e49834414b19aa596fd1d13
Artemiy <beljakovav@gmail.com>
2019-01-29 18:08:53 +0300

    ISEMS-UI 29.01.2019

---
+++ public/js/common_helpers/getRandomInt.js
@@ -0,0 +1,15 @@
+/**
+ * Возвращает произвольное число из заданного диапазона
+ * 
+ * Версия 0.1, дата релиза 29.01.2019
+ */
+
+'use strict';
+
+function randomInteger(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+}
+
+export { randomInteger };
