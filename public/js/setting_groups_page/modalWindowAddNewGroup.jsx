2ae8ffcf285083b5aa14b1a2410d906bb182e48d
Artemiy <beljakovav@gmail.com>
2019-01-31 18:31:44 +0300

    ISEMS-UI 31.01.2019

---
+++ public/js/setting_groups_page/modalWindowAddNewGroup.jsx
@@ -0,0 +1,21 @@
+/**
+ * Модуль формирования модального окна добавления нового пользователя
+ * 
+ * Версия 0.1, дата релиза 31.01.2019
+ */
+
+import React from 'react'
+import Modal from 'react-bootstrap/Modal'
+import ReactDOM from 'react-dom'
+
+class ModalWindowAddNewGroup extends React.Component {
+    constructor(props, context) {
+        super(props, context)
+    }
+
+    rander() {
+
+    }
+}
+
+export default ModalWindowAddNewGroup 
