1f69fda6e0f24a7a79c79c3fbfe5d4407438f21f
Artemiy <beljakovav@gmail.com>
2019-02-13 17:24:36 +0300

    ISEMS-UI 13.02.2019

---
+++ libs/check/checkAccessRightsPage.js
@@ -0,0 +1,19 @@
+/*
+ * Информация о правах группы к которой относится пользователь
+ *
+ * Версия 0.11, дата релиза 13.02.2019
+ * */
+
+'use strict';
+
+const models = require('../../controllers/models');
+const mongodbQueryProcessor = require('../../middleware/mongodbQueryProcessor');
+
+module.exports = function(req, cb) {
+    mongodbQueryProcessor.querySelect(models.modelSessionUserInformation, {
+        query: { session_id: req.sessionID }
+    }, (err, result) => {
+        if (err) cb(err);
+        else cb(null, result)
+    })
+};
