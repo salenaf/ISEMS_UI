1e7dd70d74010f20d8d5076afe06bf869b06fa7c
Artemiy <beljakovav@gmail.com>
2020-02-25 17:42:06 +0300

    ISEMS-UI 25.02.2020

--- spec/my_page_models/js/pages/settings/organizationsAndSources/createBodyManagementEntity.jsx
+++ spec/my_page_models/js/pages/settings/organizationsAndSources/createBodyManagementEntity.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Accordion, Button, Card, Col, Form, Row } from "react-bootstrap";
 import PropTypes from "prop-types";
 
+import { ModalWindowConfirmMessage } from "../../modalwindows/modalWindowConfirmMessage.jsx";
+
 class ShowEntityInformation extends React.Component {
     constructor(props){
         super(props);
@@ -22,13 +24,29 @@ class ShowEntityInformation extends React.Component {
         let num = 1;
         return this.props.resivedInfo.listDivision.map((item) => {
             return (
-                <Card border="dark" key={`key_division_${item.name}`}>
-                    <Accordion.Toggle as={Card.Header} eventKey={num}>{item.name}</Accordion.Toggle>
+                <Card border="dark" key={`key_division_${num}`}>
+                    <Accordion.Toggle as={Card.Header} eventKey={num}>{item.divisionName}</Accordion.Toggle>
                     <Accordion.Collapse eventKey={num++}>
                         <Card.Body>
                             <Row>
-                                <Col><Form.Control type="text" onChange={this.props.handlerInputChange} value={item.name} id={`name:${item.id}`}></Form.Control></Col>
-                                <Col><Form.Control as="textarea" onChange={this.props.handlerInputChange} value={item.physicalAddress} id={`physical_address:${item.id}`}></Form.Control></Col>
+                                <Col>
+                                    <Form.Control 
+                                        type="text" 
+                                        onChange={this.props.handlerInputChange} 
+                                        value={item.divisionName} 
+                                        isValid={item.divisionNameIsValid}
+                                        isInvalid={item.divisionNameIsInvalid}
+                                        id={`division_name:${item.id}`} />
+                                </Col>
+                                <Col>
+                                    <Form.Control 
+                                        as="textarea" 
+                                        onChange={this.props.handlerInputChange} 
+                                        value={item.physicalAddress} 
+                                        isValid={item.physicalAddressIsValid}
+                                        isInvalid={item.physicalAddressIsInvalid}
+                                        id={`physical_address:${item.id}`} />
+                                </Col>
                             </Row>
                             <Row>
                                 <Col md={6}><Form.Control as="textarea" onChange={this.props.handlerInputChange} value={item.description} id={`description:${item.id}`}></Form.Control></Col>
@@ -58,23 +76,34 @@ class ShowEntityInformation extends React.Component {
 
         let num = 1;
 
-        console.log(this.props.listFieldActivity);
-        console.log(this.props.resivedInfo);
-
         return (
             <Accordion defaultActiveKey="0" style={{ width: "55rem" }}>
                 <Card border="info">
-                    <Accordion.Toggle as={Card.Header} eventKey="0">{this.props.resivedInfo.name}</Accordion.Toggle>
+                    <Accordion.Toggle as={Card.Header} eventKey="0">{this.props.resivedInfo.organizationName}</Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
                         <Card.Body>
                             <Row>
-                                <Col><Form.Control type="text" onChange={this.props.handlerInputChange} defaultValue={this.props.resivedInfo.name} id="name"></Form.Control></Col>
-                                <Col><Form.Control as="textarea" onChange={this.props.handlerInputChange} defaultValue={this.props.resivedInfo.legalAddress} id="legal_address"></Form.Control></Col>
+                                <Col><Form.Control 
+                                    type="text" 
+                                    onChange={this.props.handlerInputChange} 
+                                    value={this.props.resivedInfo.organizationName} 
+                                    isValid={this.props.resivedInfo.organizationNameIsValid}
+                                    isInvalid={this.props.resivedInfo.organizationNameIsInvalid}
+                                    id="organization_name" />
+                                </Col>
+                                <Col>
+                                    <Form.Control 
+                                        as="textarea" 
+                                        onChange={this.props.handlerInputChange} 
+                                        value={this.props.resivedInfo.legalAddress}
+                                        isValid={this.props.resivedInfo.legalAddressIsValid} 
+                                        isInvalid={this.props.resivedInfo.legalAddressIsInvalid} 
+                                        id="legal_address" />
+                                </Col>
                             </Row>
                             <Row>
                                 <Col>
                                     <Form.Control as="select" onChange={this.props.handlerInputChange} value={this.props.resivedInfo.fieldActivity} id="field_activity" size="sm">
-                                        <option value="" key="list_field_activity_0">...</option>
                                         {list.map((item) => <option value={item} key={`list_field_activity_${num++}`}>{item}</option>)}
                                     </Form.Control>
                                 </Col>
@@ -199,21 +228,28 @@ export default class CreateBodyManagementEntity extends React.Component {
 
         this.state = {
             showInfo: false,
+            modalWindowSourceDel: false,
             listOrganizationName: this.createListOrganization.call(this),
             listDivisionName: this.createListDivision.call(this),
             objectShowedInformation: {},
         };
 
+        this.deleteEntityOptions = {};
+
         this.listSourceName = this.createListSource.call(this);
 
+        this.checkValue = this.checkValue.bind(this);
         this.handlerSave = this.handlerSave.bind(this);
         this.handlerDelete = this.handlerDelete.bind(this);
         this.handlerSelected = this.handlerSelected.bind(this);
         this.handlerInputChange = this.handlerInputChange.bind(this);
+        this.handlerEntityDelete = this.handlerEntityDelete.bind(this);
+        this.closeModalWindowSourceDel = this.closeModalWindowSourceDel.bind(this);
+        this.searchEntityInObjectShowedInformation = this.searchEntityInObjectShowedInformation.bind(this);
     }
 
     createListOrganization(){
-        console.log("createListOrganization START...");
+        //console.log("createListOrganization START...");
 
         let listTmp = {};
         for(let source in this.props.listSourcesInformation){
@@ -224,7 +260,7 @@ export default class CreateBodyManagementEntity extends React.Component {
     }
 
     createListDivision(){
-        console.log("createListDivision START...");
+        //console.log("createListDivision START...");
 
         let listTmp = {};
         for(let source in this.props.listSourcesInformation){
@@ -238,7 +274,7 @@ export default class CreateBodyManagementEntity extends React.Component {
     }
 
     createListSource(){
-        console.log("createListSource START...");
+        //console.log("createListSource START...");
 
         let listTmp = {};
         for(let source in this.props.listSourcesInformation){
@@ -250,6 +286,26 @@ export default class CreateBodyManagementEntity extends React.Component {
         }
 
         return listTmp;
+    }
+
+    searchEntityInObjectShowedInformation(type, id){
+        let osi = this.state.objectShowedInformation;
+
+        if(type === "organization"){
+            if(osi.id === id){
+                return osi;
+            }
+
+            return {};
+        }
+
+        for(let i = 0; i < osi.listDivision.length; i++){
+            if(osi.listDivision[i].id === id){
+                return osi.listDivision[i];
+            }
+        }
+
+        return {};
     }
 
     getListFieldActivity(){
@@ -279,11 +335,15 @@ export default class CreateBodyManagementEntity extends React.Component {
         for(let sourceID in lsi){
             if(lsi[sourceID][paramType[searchType]] === searchID){
                 tmp.id = lsi[sourceID].oid;
-                tmp.name = lsi[sourceID].organization.name;
+                tmp.organizationName = lsi[sourceID].organization.name;
+                tmp.organizationNameIsValid = true;
+                tmp.organizationNameIsInvalid = false;
                 tmp.dateRegister = lsi[sourceID].organization.dateRegister;
                 tmp.dateChange = lsi[sourceID].organization.dateChange;
                 tmp.fieldActivity = lsi[sourceID].organization.fieldActivity;
                 tmp.legalAddress = lsi[sourceID].organization.legalAddress;
+                tmp.legalAddressIsValid = true;
+                tmp.legalAddressIsInvalid = false;
                 tmp.listDivision = [];
     
                 oid = lsi[sourceID].oid;
@@ -303,10 +363,14 @@ export default class CreateBodyManagementEntity extends React.Component {
     
                 tmp.listDivision.push({
                     "id": lsi[sourceID].did,
-                    "name": lsi[sourceID].division.name,
+                    "divisionName": lsi[sourceID].division.name,
+                    "divisionNameIsValid": true,
+                    "divisionNameIsInvalid": false,
                     "dateRegister": lsi[sourceID].division.dateRegister,
                     "dateChange": lsi[sourceID].division.dateChange,
                     "physicalAddress": lsi[sourceID].division.physicalAddress,
+                    "physicalAddressIsValid": true,
+                    "physicalAddressIsInvalid": false,
                     "description": lsi[sourceID].division.description,
                     "listSource": sourceListTmp,
                 });
@@ -333,22 +397,9 @@ export default class CreateBodyManagementEntity extends React.Component {
     }
 
     handlerInputChange(e){
-        console.log("func 'handlerInputChange', START...");
-        console.log(`ID: ${e.target.id}, Value: ${e.target.value}`);
-
-        /**
-         * 
-         *  !!!!!!!!!!!!!!! 
-         * 
-         * Непонятно работает обработчик ввода. При изменении названия организации, название меняется,
-         * однако, при выборе из списка другой организации все поля меняются, кроме названия организации
-         * помененной ранее.
-         * При изменении названия подразделения, вводится только ОДИН символ, следующий символ можно ввести
-         * только после повторного выделения поля
-         */
-
         let pattern = {
-            "name": "name",
+            "organization_name": "organizationName",
+            "division_name": "divisionName",
             "legal_address": "legalAddress",
             "field_activity": "fieldActivity",
             "physical_address": "physicalAddress",
@@ -359,10 +410,28 @@ export default class CreateBodyManagementEntity extends React.Component {
         let [ name, ID ] = e.target.id.split(":");
         if(typeof ID === "undefined"){
             tmpObj[pattern[name]] = e.target.value;
+
+            if(this.checkValue(pattern[name], e.target.value)){
+                tmpObj[`${pattern[name]}IsValid`] = true;
+                tmpObj[`${pattern[name]}IsInvalid`] = false;
+            } else {
+                tmpObj[`${pattern[name]}IsValid`] = false;
+                tmpObj[`${pattern[name]}IsInvalid`] = true;
+            }
         } else {
             for(let i = 0; i < tmpObj.listDivision.length; i++){
                 if(tmpObj.listDivision[i].id === ID){
                     tmpObj.listDivision[i][pattern[name]] = e.target.value;
+
+                    if(this.checkValue(pattern[name], e.target.value)){
+                        tmpObj.listDivision[i][`${pattern[name]}IsValid`] = true;
+                        tmpObj.listDivision[i][`${pattern[name]}IsInvalid`] = false;
+                    } else {
+                        tmpObj.listDivision[i][`${pattern[name]}IsValid`] = false;
+                        tmpObj.listDivision[i][`${pattern[name]}IsInvalid`] = true;
+                    }
+
+                    break;
                 }
             }
         }
@@ -372,14 +441,89 @@ export default class CreateBodyManagementEntity extends React.Component {
 
     handlerSave(entityType, entityID){
         console.log("func 'handlerSave', START...");
-        console.log(`отправляем серверу запрос для 'СОХРАНЕНИЯ' информации в БД, ${JSON.stringify({ action: "entity_save", entityType: entityType, entityID: entityID })}`);
 
+        let entityInfo = this.searchEntityInObjectShowedInformation(entityType, entityID);
+
+        if(entityType === "organization"){
+
+            //проверяем что бы поля были валидны
+            if(entityInfo.organizationNameIsInvalid || entityInfo.legalAddressIsInvalid){
+                return;
+            }
+
+            let reqOrg = JSON.stringify({ 
+                action: "entity_save", 
+                entityType: entityType, 
+                entityID: entityID,
+                options: {
+                    organizationName: entityInfo.organizationName,
+                    fieldActivity: entityInfo.fieldActivity,
+                    legalAddress: entityInfo.legalAddress,
+                }});
+
+            console.log(`отправляем серверу запрос для 'СОХРАНЕНИЯ' информации об организации, ${reqOrg}`);
+
+            return;
+        } 
+
+        //проверяем что бы поля были валидны
+        if(entityInfo.divisionNameIsInvalid || entityInfo.physicalAddressIsInvalid){
+            return;
+        }
+
+        let reqDiv = JSON.stringify({ 
+            action: "entity_save", 
+            entityType: entityType, 
+            entityID: entityID,
+            options: {
+                divisionName: entityInfo.divisionName,
+                physicalAddress: entityInfo.physicalAddress,
+                description: entityInfo.description,
+            }});
+
+        console.log(`отправляем серверу запрос для 'СОХРАНЕНИЯ' информации об подразделении, ${reqDiv}`);
     }
 
     handlerDelete(entityType, entityID){
-        console.log("func 'handlerSave', START...");
-        console.log(`отправляем серверу запрос для 'УДАЛЕНИЯ' информации из БД, ${JSON.stringify({ action: "entity_save", entityType: entityType, entityID: entityID })}`);
+        let entityInfo = this.searchEntityInObjectShowedInformation(entityType, entityID);
 
+        let entityName = (entityType === "organization")? entityInfo.organizationName: entityInfo.divisionName;
+        this.deleteEntityOptions = {
+            entityType: entityType,
+            entityID: entityID,
+            name: entityName,
+        };
+
+        this.setState({ modalWindowSourceDel: true });
+    }
+
+    handlerEntityDelete(){
+        let request = JSON.stringify({ action: "entity_delete", entityType: this.deleteEntityOptions.entityType, entityID: this.deleteEntityOptions.entityID });
+
+        /**
+ * В БД сделать проверку на наличие дочерних потомков у удаляемой сущности. Если есть потомки то удаление не производится,
+ * выводится информационное сообщение о невозможности удалить сущност у которой есть потомки
+ */
+
+        console.log(`отправляем серверу запрос для 'УДАЛЕНИЯ' информации из БД, ${request}`);
+        
+        this.closeModalWindowSourceDel();
+    }
+
+    closeModalWindowSourceDel(){
+        this.setState({ modalWindowSourceDel: false });
+    }
+
+    checkValue(nameInput, value){
+        console.log("func 'checkValue', проверяем передоваемое значение");
+        console.log(value);
+
+        //только для теста
+        if(value.includes("&")){
+            return false;
+        }
+
+        return true;
     }
 
     render(){
@@ -411,6 +555,14 @@ export default class CreateBodyManagementEntity extends React.Component {
                     listFieldActivity={this.getListFieldActivity.call(this)}
                     resivedInfo={this.state.objectShowedInformation}
                     handlerInputChange={this.handlerInputChange} />
+                <ModalWindowConfirmMessage 
+                    show={this.state.modalWindowSourceDel}
+                    onHide={this.closeModalWindowSourceDel}
+                    msgBody={`Вы действительно хотите удалить ${(this.deleteEntityOptions.entityType === "organization") ? "организацию":"подразделение"} '${this.deleteEntityOptions.name}'`}
+                    msgTitle={"Удаление"}
+                    nameDel=""
+                    handlerConfirm={this.handlerEntityDelete}
+                />
             </React.Fragment>
         );
     }
