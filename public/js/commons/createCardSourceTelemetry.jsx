089a30f019b674b4d4eed54513f18ccc608a6286
Artemiy <beljakovav@gmail.com>
2020-12-23 15:18:58 +0300

    ISEMS-UI 23.12.2020 (не полностью, но сделал страницу телеметрии)

--- public/js/commons/createCardSourceTelemetry.jsx
+++ public/js/commons/createCardSourceTelemetry.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Badge, Col, Row, } from "react-bootstrap";
 import { makeStyles } from "@material-ui/core/styles";
-//import Accordion from '@material-ui/core/Accordion';
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
 import Skeleton from "@material-ui/lab/Skeleton";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import WarningIcon from "@material-ui/icons/Warning";
+import { yellow } from "@material-ui/core/colors";
 import PropTypes from "prop-types";
 
 import {helpers} from "../common_helpers/helpers.js";
@@ -27,6 +37,12 @@ const useStyles = makeStyles({
     pos: {
         marginBottom: 12,
     },
+    details: {
+        alignItems: "center",
+    },
+    column: {
+        flexBasis: "33.33%",
+    },
 });
 
 export default function CreateCardSourceTelemetry(props) {
@@ -40,6 +56,8 @@ export default function CreateCardSourceTelemetry(props) {
         hour: "numeric",
         minute: "numeric",
     });
+
+    const numFormatter = new Intl.NumberFormat("ru");
 
     const handleClose = function(sid, e) {
         props.handleClose(sid);
@@ -71,12 +89,142 @@ export default function CreateCardSourceTelemetry(props) {
         }
     };
 
+    const createStoragesDiskSpace = () => {
+        let storageTimeInterval = props.sourceInfo.informationTelemetry.timeInterval;
+        let dateMin = 0;
+        let dateMax = 0;
+        let listStorages = [];
+
+        for(let key in storageTimeInterval){
+            if(dateMin === 0 || dateMin > storageTimeInterval[key].dateMin){
+                dateMin = storageTimeInterval[key].dateMin;
+            }
+
+            if(dateMax < storageTimeInterval[key].dateMax){
+                dateMax = storageTimeInterval[key].dateMax;
+            }
+
+            listStorages.push(<Row key={`key_${key}`}>
+                <Col md={6} className="text-left">
+                    <Typography variant="body2" component="p">{key}</Typography>
+                </Col>
+                <Col md={3} className="text-right">
+                    <Typography variant="body2" component="p">
+                        <i>{formatter.format(storageTimeInterval[key].dateMin)}</i>
+                    </Typography>
+                </Col>
+                <Col md={3} className="text-right">
+                    <Typography variant="body2" component="p">
+                        <i>{formatter.format(storageTimeInterval[key].dateMax)}</i>
+                    </Typography>
+                </Col>
+            </Row>);
+        }
+
+        let dateTimeBegin = formatter.format(dateMin);
+        let dateTimeEnd = formatter.format(dateMax);
+        let timeStorageFiles = ((dateMax - dateMin) / 86400000).toFixed(1);
+        let behindCurrentTime = (+new Date - dateMax) / 3600000;
+
+        let iconWarning = "";
+        let behindCurrentTimeColor = ""; 
+        
+        if(behindCurrentTime > 12) {
+            iconWarning = <WarningIcon className="mt-n1" style={{ color: yellow[700] }} fontSize="small" />;
+            behindCurrentTimeColor = "text-danger";
+        }
+
+        return (
+            <React.Fragment>
+                <Row>
+                    <Col md={12} className="text-left mt-2">
+                        <Typography variant="body2" component="p">
+                        временной диапазон хранящихся файлов: с <i>{dateTimeBegin}</i> по <i>{dateTimeEnd}</i>
+                        </Typography>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12} className="text-left">
+                        <Typography variant="body2" component="p">
+                        среднее время хранящихся файлов: <strong>{timeStorageFiles}</strong> сут.
+                        </Typography>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12} className="text-left  mb-2">
+                        <Typography variant="body2" component="p">
+                        отставание от текущего времени: <strong className={behindCurrentTimeColor}>{behindCurrentTime.toFixed(1)}</strong> ч. {iconWarning}
+                        </Typography>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12}>{listStorages}</Col>
+                </Row>
+            </React.Fragment>
+        );
+    };
+
     const createLocalDiskSpace = () => {
-        return null;
+        let list = props.sourceInfo.informationTelemetry.diskSpace.map((item) => {
+            let used = parseInt(item.used, 10);
+
+            return (<TableRow key={`key_disk_name_${item.diskName}`}>
+                <TableCell>{item.diskName}</TableCell>
+                <TableCell>{item.mounted}</TableCell>
+                <TableCell align="right">{item.maxSpace}</TableCell>
+                <TableCell align="right">{getLevelColor(used)}%</TableCell>    
+            </TableRow>);
+        });
+
+        return (
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1c-content"
+                    id="panel1c-header" >                               
+                    <Typography variant="body2">локальные жесткие диски</Typography>
+                </AccordionSummary>
+                <Table size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell><strong>имя</strong></TableCell>
+                            <TableCell><strong>точка монтирования</strong></TableCell>
+                            <TableCell align="right"><strong>размер</strong></TableCell>
+                            <TableCell align="right"><strong>занято</strong></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>{list}</TableBody>
+                </Table>
+            </Accordion>
+
+        );
     };
 
     const createNetworkIntarface = () => {
-        return null;
+        let loadNetwork = props.sourceInfo.informationTelemetry.loadNetwork;
+
+        let list = [];
+        for(let ifname in loadNetwork){
+            list.push(<Row key={`key_${ifname}`}>
+                <Col md={4} className="text-left"><Typography variant="body2">{ifname}</Typography></Col>
+                <Col md={4} className="text-left"><Typography variant="body2">RX: {numFormatter.format(loadNetwork[ifname].RX)}</Typography></Col>
+                <Col md={4} className="text-left"><Typography variant="body2">TX: {numFormatter.format(loadNetwork[ifname].TX)}</Typography></Col>
+            </Row>);
+        }
+
+        return (
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1c-content"
+                    id="panel1c-header" >
+                    <Typography variant="body2">сетевые соединения</Typography>
+                </AccordionSummary>
+                <Row>
+                    <Col md={12} className="ml-2 mr-2 mb-2">{list}</Col>
+                </Row>
+            </Accordion>
+        );
     };
 
     const createCardBody = () => {
@@ -104,15 +252,13 @@ export default function CreateCardSourceTelemetry(props) {
                     </Col>
                 </Row>
                 <Row>
-                    <Col md={6}>{createLocalDiskSpace()}</Col>
-                    <Col md={6}>{createNetworkIntarface()}</Col>
+                    <Col md={0}>{createStoragesDiskSpace()}</Col>
                 </Row>
-                <Row className="mt-4">
-                    <Col md={12} className="text-center">
-                        <Typography variant="body2" component="p">
-                            {JSON.stringify(tele)}
-                        </Typography>
-                    </Col>
+                <Row className="mt-2">
+                    <Col md={12}>{createLocalDiskSpace()}</Col>
+                </Row>
+                <Row className="mt-2">
+                    <Col md={12}>{createNetworkIntarface()}</Col>
                 </Row>
             </React.Fragment>
         );
